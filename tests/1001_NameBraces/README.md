4f8f43db6975c135d983e75f1095e1fd928068bf
Pedro Mendes <mendes@copasi.org>
2025-09-11 20:44:42 -0400

    test for species in more than 1 compartment

---
+++ tests/1001_NameBraces/README.md
@@ -0,0 +1,8 @@
+# 1001_NameBraces
+
+Copasi model with species names that contain braces (same name different compartment)
+
+## tests
+
+- cps file reads well
+- whether the braces cause problems in parsing
---
+++ tests/1001_NameBraces/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# work out our folder name
+test=${PWD##*/}          # to assign to a variable
+test=${test:-/}          # to correct for the case where PWD=/
+
+fail=0
+
+# run sbmodelr
+sbmodelr -t a ../sources/ATPlocs.cps 2 1> output 2> /dev/null
+
+# compare output and target
+difference=$(diff output target_stdout)
+if [[ $difference ]]; then
+  printf 'FAIL %s\n' "${test}"
+  fail=1
+fi
+
+exit $fail
